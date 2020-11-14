60f782bf6217948a3b7423c7323719676832d623
liaoweibin <liaowbin@126.com>
2020-11-14 10:52:47 +0800

    Site updated: 2020-11-14 10:52:46

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?9635a427fa2d4d309e7dd1c4bfa58352";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
